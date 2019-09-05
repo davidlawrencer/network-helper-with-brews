4c1a21857e9a11520ec68d28078c28b0b9eb1533
davidlawrencer <davidlrifkin+github@gmail.com>
2019-09-05 14:15:55 -0400

    Adds image helper

--- brewery-app-with-wrappers/AppError.swift
+++ brewery-app-with-wrappers/AppError.swift
@@ -16,5 +16,6 @@ enum AppError: Error {
     case badUrl
     case notFound //404 status code
     case unauthorized //403 and 401 status code
+    case badImageData
     case other(errorDescription: String)
 }
